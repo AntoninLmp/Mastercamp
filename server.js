996f33d375133059fbd63b5283b5b6290aba7bc2
AstrydCasimir <astryd.casimirgressier@gmail.com>
2022-06-14 14:53:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -34,4 +34,4 @@ app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }));
 //ADD the routes
 
 //app.use("/static", express.static(__dirname + '/static'));
-//app.use("/home", require("./controllers/home.route"));
+app.use("/home", require("./controllers/home.route"));
