0aa71462c337e88eea509b0c8f9ba7724abb40a7
AstrydCasimir <astryd.casimirgressier@gmail.com>
2022-06-16 15:04:03 +0200

    Add routes inscriptionn

--- controllers/inscription.route.js
+++ controllers/inscription.route.js
@@ -6,4 +6,16 @@ router.get("/", (request, response) => {
     response.render("inscription_choix");
 });
 
+router.get('/patient',  (request, response) => {
+    response.render("inscription_patient");
+});
+
+router.get('/medecin',  (request, response) => {
+    response.render("inscription_sante");
+});
+
+router.get('/organismesante',  (request, response) => {
+    response.render("inscription_organisme_sante");
+});
+
 module.exports = router;
