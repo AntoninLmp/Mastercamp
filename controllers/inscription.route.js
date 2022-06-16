933651bd7ea4402758458101ad285e37480a2009
Antonin Lampin <antonin.lampin@efrei.net>
2022-06-16 13:30:34 +0200

    Updates pages inscriptions

--- controllers/inscription.route.js
+++ controllers/inscription.route.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (request, response) => {
-    response.render("inscription");
+    response.render("inscription_choix");
 });
 
 module.exports = router;
