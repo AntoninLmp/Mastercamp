5bd695a3994c524add6b7f046c28145a4239979b
Antonin Lampin <antonin.lampin@efrei.net>
2022-06-17 10:24:46 +0200

    Update inscription.js

--- static/js/inscription.js
+++ static/js/inscription.js
@@ -8,16 +8,3 @@ if (psw_1 != "" && psw_1.value == psw_2.value) {
     psw_1.style.border = "1px solid green";
     psw_2.style.border = "1px solid green";
 }
-
-
-const p_article = document.getElementById("p_art_disapear1")
-
-function myFunction(mediaQuery) {
-    if (mediaQuery.matches) { // If media query matches
-        p_article.style.display = "none";
-    }
-}
-
-const mediaQuery = window.matchMedia("(max-width: 1000px)")
-myFunction(mediaQuery) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
