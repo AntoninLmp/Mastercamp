8b0078b7220176140c4d660478cc05c952d75ef9
AstrydCasimir <astryd.casimirgressier@gmail.com>
2022-06-24 13:58:23 +0200

    Add Route To Patient

--- controllers/patient.route.js
+++ controllers/patient.route.js
@@ -5,32 +5,17 @@ const router = express.Router();
 
 const auth = require("../utils/users.auth");
 const userRepo = require("../utils/users.repository");
-
-router.get("/", auth.checkAuthentication("PATIENT"), studentHomeAction);
-async function studentHomeAction(request, response){
-  let userData = await userRepo.getOneUser(request.user.email);
-  console.log(userData);
-  console.log(request.user.email);
-  response.send("ok");
-}
-
-/*
-const express = require("express");
-const router = express.Router();
-//const ordonnanceRepo = require('../utils/ordonnance.repository');
+const ordonnanceRepo = require('../utils/ordonnance.repository');
 
 
-router.get("/", async function (request, response) {
-    console.log(request.session.passport);
-    console.log(request.isAuthenticated());
-    console.log(express.User);
+router.get("/", auth.checkAuthentication("PATIENT"), async function (request, response) {
+    console.log(request.user.email);
     var myContent = [];
-    //var ordonnance = await ordonnanceRepo.getAllOrdonnance()
+    var ordonnance = await ordonnanceRepo.getAllOrdonnance()
     var flashMessage = request.session.flashMessage;
     request.session.flashMessage = "";
-    //response.render("patient_home.ejs", { "ordonnances": ordonnance, "flashMessage": flashMessage });
-    response.send("OKAY");
-});*/
+    response.render("patient_home.ejs", { "ordonnances": ordonnance, "flashMessage": flashMessage });
+});
 
 
 module.exports = router;
