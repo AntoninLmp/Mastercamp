999657eb49ab1ca0a8e495d4b2ddc46e65f9f5d6
Antonin Lampin <antonin.lampin@efrei.net>
2022-06-30 14:31:44 +0200

    Update

--- controllers/organismesante.route.js
+++ controllers/organismesante.route.js
@@ -10,28 +10,28 @@ const pharmaRepo = require('../utils/pharmacie.repository');
 router.get("/", auth.checkAuthentication("PHARMACIE"), async function (request, response) {
     var pharmacie = await pharmaRepo.getOnePharma(request.user.email);
     var ordonnances = [];
-    var ordonnance =[];
+    var ordonnance = [];
     console.log(ordonnance.length);
-    response.render("orgasante_home.ejs", {"ordonnance": ordonnance, "ordonnances": ordonnances, "pharma": pharmacie});
+    response.render("orgasante_home.ejs", { "ordonnance": ordonnance, "ordonnances": ordonnances, "pharma": pharmacie });
 });
 
 router.post("/searchByIdOrdonnance", auth.checkAuthentication("PHARMACIE"), SearchByIdOrdonnance)
-async function SearchByIdOrdonnance(request, response){
+async function SearchByIdOrdonnance(request, response) {
     var pharmacie = await pharmaRepo.getOnePharma(request.user.email);
     var ordonnances = [];
     var ordonnance = await ordonnanceRepo.getOneOrdonnance(request.body.numeroOrdo);
     console.log(ordonnance);
-    response.render("orgasante_home", { "ordonnance": ordonnance, "ordonnances": ordonnances, "pharma": pharmacie});
+    response.render("orgasante_home", { "ordonnance": ordonnance, "ordonnances": ordonnances, "pharma": pharmacie });
 };
 
 
 router.post("/searchByPatientOrdonnance", auth.checkAuthentication("PHARMACIE"), searchByPatientOrdonnance)
-async function searchByPatientOrdonnance(request, response){
+async function searchByPatientOrdonnance(request, response) {
     var pharmacie = await pharmaRepo.getOnePharma(request.user.email);
     var ordonnance = [];
     var ordonnances = await ordonnanceRepo.getAllOrdonnanceByPatientBySecu(request.body.numeroSecu);
     console.log(ordonnances);
-    response.render("orgasante_home", { "ordonnance": ordonnance, "ordonnances": ordonnances, "pharma": pharmacie});
+    response.render("orgasante_home", { "ordonnance": ordonnance, "ordonnances": ordonnances, "pharma": pharmacie });
 };
 
 router.post("/updatePharmacie", auth.checkAuthentication("PHARMACIE"), updateUser);
