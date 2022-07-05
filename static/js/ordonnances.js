74b316ef20f99201c5f1f04e04aa4a025728743c
Antonin Lampin <antonin.lampin@efrei.net>
2022-07-05 10:18:18 +0200

    Front recherche

--- static/js/ordonnances.js
+++ static/js/ordonnances.js
@@ -3,11 +3,11 @@ const ordosuivantes = document.getElementById("ordosuivantes");
 
 ordosuivantes.style.display = "none";
 
-function togg(){
-    if(getComputedStyle(ordosuivantes).display != "none"){
+function togg() {
+    if (getComputedStyle(ordosuivantes).display != "none") {
         ordosuivantes.style.display = "none";
     } else {
-        ordosuivantes.style.display = "block";
+        ordosuivantes.style.display = "flex";
     }
-  };
-  afficherlasuite.onclick = togg;
+};
+afficherlasuite.onclick = togg;
