6ebed00405432e6e7d582db3987cf91cd015dc32
Chloerlld123 <73694451+Chloerlld123@users.noreply.github.com>
2022-07-07 00:04:42 +0200

    about us page + header root

--- controllers/home.route.js
+++ controllers/home.route.js
@@ -8,4 +8,16 @@ function homeRootAction(response){
     response.render("home");
 }
 
+router.get('/contact', contactRootAction);
+//http://localhost:9000
+function contactRootAction(request, response){
+    response.render("contact");
+}
+
+router.get('/aboutus', aboutusRootAction);
+//http://localhost:9000
+function aboutusRootAction(request, response){
+    response.render("aboutus");
+}
+
 module.exports = router;
